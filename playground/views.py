e5f54e335c21e8b420afd82a1630b08ddb7beeab
blackshiinobi <blackshiinobi@gmail.com>
2022-03-23 22:34:25 +0100

    Finished orm section

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from store.models import Product, Order
-from django.db.models.aggregates import Count, Max, Min, Sum, Avg
+from store.models import Order, OrderItem
+from django.db import transaction
 
 
 def say_hello(request):
-    result = Product.objects.filter(collection__id=3).aggregate(count=Count('id'), min_price=Min('unit_price'))
 
-    return render(request, 'hello.html', {'name': 'Youssef', 'result': result})
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
+
+    return render(request, 'hello.html', {'name': 'Youssef'})
