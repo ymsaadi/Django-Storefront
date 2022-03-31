c49ce253b194b21d541c0ccfe4d5be5f7a4361e8
ymsaadi <y.msaadi@harmony-technology.net>
2022-03-31 16:28:53 +0000

    Model modifications

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from uuid import uuid4
 
 
 class Promotion(models.Model):
@@ -68,13 +69,17 @@ class OrderItem(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
 
 
 class Address(models.Model):
